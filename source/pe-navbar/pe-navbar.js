7a2b542a826d49739aae7a53d40aad7dd4769e9e
Iain Reid <iainreid382@gmail.com>
2016-10-13 22:13:15 +0100

    [chore] Refactoring directory structure

---
+++ source/pe-navbar/pe-navbar.js
@@ -0,0 +1,11 @@
+'use strict'
+
+class PeNavbar {
+  ready() {
+    console.log('PE Navbar ready')
+
+    this.title = 'Polymer Example'
+  }
+}
+
+Polymer(PeNavbar)
---
+++ source/pe-root/pe-root.js
@@ -0,0 +1,9 @@
+'use strict'
+
+class PeRoot {
+  ready () {
+    console.log('PE Root ready')
+  }
+}
+
+Polymer(PeRoot)
